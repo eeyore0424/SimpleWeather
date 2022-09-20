50bf8367159385c25934b1e7d0dd233ae07a3ea8
eeyore <eeyore0424@gmail.com>
2022-09-21 07:23:11 +0900

    Complete UI stroyboard

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var changeButtonTapped: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
     }
     
+    let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
+    
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+            
+            print("도시, 온도, 날씨 이미지 변경 !")
+        }
+        
 
     /*
     // MARK: - Navigation
